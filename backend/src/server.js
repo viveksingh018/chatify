6d79078a45a634810e59eb5e6e25b93af617cb23
Vivek Singh <vivekkumarhcc245@gmail.com>
2026-05-21 11:54:52 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
+
+app.listen(PORT, () => console.log("Server is running on port 3000 "))
+
